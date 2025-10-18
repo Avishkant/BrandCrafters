6cee40bf7810a33fe1f737f073a0db7f60af9982
Avishkant <avishkantpatidar22@gmail.com>
2025-10-18 12:14:59 +0530

    Added case studies page and readmore inside it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
 import Home from './pages/Home'
 import CaseStudies from './pages/CaseStudies'
+import ReadMore from './pages/ReadMore'
 import Footer from './components/Footer'
 
 function App(){
   const path = typeof window !== 'undefined' ? window.location.pathname : '/'
+
+  if (path === '/readmore') {
+    return (
+      <>
+        <ReadMore />
+        <Footer />
+      </>
+    )
+  }
 
   return (
     <>
